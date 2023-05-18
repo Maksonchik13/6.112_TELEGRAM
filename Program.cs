7862332ab8b23e5f51661638261ebc32d2b8a13e
Пользователь <@-PC>
2023-05-18 17:38:11 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("введите число ");
+long n=long.Parse(Console.ReadLine());
+int index=0;
+int count = 0;
+int proverka = 0;
+while (n > 0)
+{
+    long p = n % 10;
+    n /= 10;
+    count++;
+    if (p == 3 && proverka == 0) 
+    {
+    proverka++;
+        index = count;
+    }
+}
+Console.WriteLine($"номер цифры '3' с конца = {index}") ;
